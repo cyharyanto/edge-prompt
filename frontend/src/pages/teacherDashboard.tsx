ffa53a008724c1a1eb8504c2af35fdbb8322bae3
Harriet2004 <s3933558@student.rmit.edu.au>
2025-04-22 15:17:45 +1000

    Made a seperate file for the teacher dashboard

---
+++ frontend/src/pages/teacherDashboard.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const teacherDashboard: React.FC = () => {
+    return (<h2>Welcome!</h2>);
+};
+
+export default teacherDashboard;
